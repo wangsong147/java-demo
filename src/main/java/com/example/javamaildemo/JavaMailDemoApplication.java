78b3053f13c296752191a105dde39103c0e09a2b
ws <18945588017@163.com>
2023-02-06 21:40:13 +0800

    l

--- src/main/java/com/example/javamaildemo/JavaMailDemoApplication.java
+++ src/main/java/com/example/javamaildemo/JavaMailDemoApplication.java
@@ -1,13 +1,34 @@
 package com.example.javamaildemo;
 
+import com.example.javamaildemo.vo.MailVo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 @SpringBootApplication
 public class JavaMailDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaMailDemoApplication.class, args);
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(5);
+        integers.add(6);
+        integers.add(1);
+        integers.add(4);
+        integers.add(2);
+        integers.add(3);
+        integers.stream().sorted(Comparator.comparing(x -> x)).forEach(System.out::println);
+
+        List<MailVo> mailVos = new ArrayList<>();
+        mailVos.add(new MailVo(5L,"from","to"));
+        mailVos.add(new MailVo(6L,"from","to"));
+        mailVos.add(new MailVo(1L,"from","to"));
+        mailVos.add(new MailVo(4L,"from","to"));
+        mailVos.stream().sorted(Comparator.comparing(MailVo::getId)).forEach(System.out::println);
     }
 
 }
--- src/main/java/com/example/javamaildemo/vo/MailVo.java
+++ src/main/java/com/example/javamaildemo/vo/MailVo.java
@@ -1,9 +1,14 @@
 package com.example.javamaildemo.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MailVo {
+    private Long id;
     private String fromMail;
     private String to;
 }
