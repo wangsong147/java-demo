fe88ea29db2d2ab567523fd0590b5a5c17171cae
ws <18945588017@163.com>
2023-02-06 21:59:45 +0800

    l

--- src/main/java/com/example/javamaildemo/JavaMailDemoApplication.java
+++ src/main/java/com/example/javamaildemo/JavaMailDemoApplication.java
@@ -22,6 +22,7 @@ public class JavaMailDemoApplication {
         integers.add(2);
         integers.add(3);
         integers.stream().sorted(Comparator.comparing(x -> x)).forEach(System.out::println);
+        System.out.println("\n");
 
         List<MailVo> mailVos = new ArrayList<>();
         mailVos.add(new MailVo(5L,"from","to",5));
@@ -29,11 +30,10 @@ public class JavaMailDemoApplication {
         mailVos.add(new MailVo(1L,"from","to",1));
         mailVos.add(new MailVo(4L,"from","to",4));
         // Comparator静态方法
-        mailVos.stream().sorted(Comparator.comparing(MailVo::getId).reversed()).forEach(System.out::println);
-        System.out.println("\n");
+        mailVos.stream().sorted(Comparator.comparing(MailVo::getId).reversed()).forEach(System.out::println);System.out.println("\n");
         // 对象方法
-        mailVos.stream().sorted((x,y)->Long.compare(y.getId(),x.getId())).forEach(System.out::println);
-        System.out.println("\n");
+        mailVos.stream().sorted((x,y)->Long.compare(y.getId(),x.getId())).forEach(System.out::println);System.out.println("\n");
+
         mailVos.stream().sorted((x,y)->y.getAge()-x.getAge()).forEach(System.out::println);
     }
 
