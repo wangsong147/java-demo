2f780c3245c40b5f751b485fe1d4ae4bc8fbf844
wangsong <song5.wang@yidatec.com>
2023-03-17 15:00:02 +0800

    2023-3-17

--- src/main/java/com/example/javamaildemo/controller/SecurityController.java
+++ src/main/java/com/example/javamaildemo/controller/SecurityController.java
@@ -1,29 +1,36 @@
 package com.example.javamaildemo.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/seccurity")
+@RequestMapping("/security")
 public class SecurityController {
+    @Secured({"ROLE_teacher", "ROLE_student"})
     @GetMapping("/test1")
     public void test1() {
     }
 
+    @PreAuthorize("hasAnyAuthority('admins','student')")
     @GetMapping("/test2")
     public void test2() {
     }
 
+    @PostAuthorize("hasAnyAuthority('rapper')")
     @GetMapping("/test3")
     public void test3() {
     }
 
-    @GetMapping("/test4")
+    @PostMapping("/login")
     public void test4() {
     }
 
-    @GetMapping("/test5")
+    @GetMapping("/logout")
     public void test5() {
     }
 }
